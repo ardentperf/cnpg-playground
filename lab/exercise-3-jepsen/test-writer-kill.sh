f521c0319626ac9311ac57a172379c2fdd981e5e
Jeremy Schneider <schneider@ardentperf.com>
2025-09-07 17:46:59 -0700

    command to clear out jepsen results from minio

--- lab/exercise-3-jepsen/test-writer-kill.sh
+++ lab/exercise-3-jepsen/test-writer-kill.sh
@@ -8,7 +8,9 @@
 # spec from exercise 3
 #
 #
-#   TEST_NAME=writer-kill-async
+#   docker exec minio-eu rm -rf /data/jepsenpg      (make sure you've downloaded previous test results before running this)
+#
+#   TEST_NAME=writer-kill-async                     (or writer-kill-sync if you enabled sync replication)
 #   mkdir $HOME/jepsen-test-${TEST_NAME}
 #   cd $HOME/jepsen-test-${TEST_NAME}
 #   cp $HOME/cnpg-playground/lab/exercise-3-jepsen/test-writer-kill.sh ./
