e8a7d22b06e9185d5557976b8f704c8fbff11304
Jeremy Schneider <schneider@ardentperf.com>
2025-09-07 23:23:26 -0700

    default postgres containers only

--- lab/exercise-3-jepsen/docker-network-delay.sh
+++ lab/exercise-3-jepsen/docker-network-delay.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-#
-# this script adjusts the network latency between all Docker containers to ~1ms RTT
-# by applying 0.5ms netem delay on each container's eth0 interface. Optionally, a
-# different per-container delay can be specified.
-#
-# WARNING: this affects traffic for all containers (and thus any pods running
-# inside kind node containers). Intended for testing only!
-#
 
 if [ "$1" != "on" ] && [ "$1" != "off" ] && [ "$1" != "check" ]; then
   echo "Usage: bash docker-network-delay.sh [on|off|check] [ms=0.5]"
@@ -15,14 +7,17 @@ fi
 
 MS=${2:-0.5}
 
+kubectl config use-context kind-k8s-eu
+
 # check current latency
 kubectl run pingtest --image busybox:1.36 --overrides='{"spec": {"nodeName": "k8s-eu-worker3"}}' \
               --restart=Never --rm -it -- ping -c 5 k8s-eu-worker4 || exit
 
 [ "$1" == "check" ] && exit 0
 
-# set network latency to 0.5ms per direction on all containers; total ~1ms RTT
-CONTAINERS=$(docker ps --format '{{.Names}}' | tr -d '\r' || true)
+# set network latency
+CONTAINERS=$(kubectl get node -l postgres.node.kubernetes.io -o jsonpath='{.items[*].metadata.name}')
+#CONTAINERS=$(docker ps --format '{{.Names}}' | tr -d '\r' || true)     # uncomment for all containers
 for c in $CONTAINERS; do
   if [ "$1" == "on" ]; then
     echo "Applying $MS ms delay to container: $c (eth0)"
