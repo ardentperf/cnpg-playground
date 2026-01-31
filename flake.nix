b29dddf87fd7793251c19888e4ce481b328beeeb
Jeremy Schneider <schneider@ardentperf.com>
2026-01-31 14:51:52 -0800

    add go-task and dagger

--- flake.nix
+++ flake.nix
@@ -4,9 +4,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    dagger.url = "github:dagger/nix";
+    dagger.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, dagger, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
     pkgs = import nixpkgs {inherit system; };
@@ -49,6 +51,8 @@
         pkgs.bat
         pkgs.lazygit
         pkgs.gh
+        pkgs.go-task
+        dagger.packages.${system}.dagger
      ];
     };
   });
